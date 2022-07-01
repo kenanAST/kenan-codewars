dc0aa1fe63bc43a24ec1d89c7d0a2d66956780b2
Kenan Cinches <kenan@beautitag.com>
2022-07-01 12:15:30 +0800

    feat: added javascript top rated digital root solution

--- javascript/digital-root.js
+++ javascript/digital-root.js
@@ -2,6 +2,11 @@ const digital_root = (n) => {
 	return n % 9 === 0 && n != 0 ? 9 : n % 9;
 };
 
+//Top Rated Solution
+const top_solution_digital_root = (n) => {
+	return ((n - 1) % 9) + 1;
+};
+
 console.log(digital_root(0));
 console.log(digital_root(5));
 console.log(digital_root(27));
